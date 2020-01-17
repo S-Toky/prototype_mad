080cc4bba4c067e0e1cde7b7421e167f04cde5ee
S-Toky <stokyr29@gmail.com>
2020-01-17 16:01:16 +0300

    add etoile

--- calcul.js
+++ calcul.js
@@ -1,4 +1,26 @@
-document.getElementById("panier").addEventListener("click", function () {
-    var pani = document.getElementById("pani")
-    pani.innerHTML = (1);
+document.addEventListener("click", function () {
+    
+    var curcuma = document.getElementById("panier1");
+    var gingembre = document.getElementById("panier2");
+    var muscade = document.getElementById("panier3");
+    var baobab = document.getElementById("panier4");
+    var fsv = document.getElementById("panier5");
+    var fsb = document.getElementById("panier6");
+    var voatsipy = document.getElementById("panier7")
+    var pani = document.getElementById("pani");
+
+
+
+    var str = pani.innerHTML;
+    var sp = document.getElementById("panier");
+    for (var i = 0; i < sp.length; i++) {
+        if (sp[i].style.backgroundColor != "") {
+            sp[i].setAttribute("back", sp[i].style.backgroundColor);
+        }
+        if (sp[i].getAttribute("style") != null) {
+            str = str.replace(sp[i].outerHTML, sp[i].innerHTML);
+        }
+    }
+    pani.innerHTML = str;
+
 });
